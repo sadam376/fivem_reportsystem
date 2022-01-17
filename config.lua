74848906b1e86e99e47d5fb83e4bd764353d383a
Adam <87525637+sadam376@users.noreply.github.com>
2022-01-17 19:34:07 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,19 @@
+-- A script nevét ne módosítsd!
+Config = {}
+
+
+--Funkciók
+Config.ShowReplyMessageForAdmins = false --Az Admin válasz üzenet részének kijelzése (csak az adminoknak)
+Config.ShowReplyMessageForAdmin = false --Az Admin válasz üzenet részének kijelzése (csak neked)
+
+Config.DeleteReport = true --Reportok törlése x idő közönként
+Config.DeleteReportTime = 200000 -- Reportok törlésének az ideje 
+
+
+
+--Parancsok
+Config.ReplyCommand = "r" --Válasz parancs
+Config.ReportCommand = "report" -- Report írás parancs
+Config.ToggleReport = "togreport" --Report megjelenés ki /be kapcsolásának a parancsa
+
+-- A script nevét ne módosítsd!
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'FiveM Land (Adam) | https://discord.gg/Zdaqj5EM5Z'
+description 'Simple report system for ESX LEGACY'
+version '1.0.0'
+
+
+--Server 
+server_scripts {
+    'server.lua',
+}
+
+--Shared 
+shared_scripts {
+    'config.lua',
+    '@es_extended/imports.lua',
+}
