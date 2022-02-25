457568c6f88f9ce0103905a5efd0964c973e2913
Adam <87525637+sadam376@users.noreply.github.com>
2022-02-25 19:39:49 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,11 +5,6 @@ Config = {}
 Config.ShowReplyMessageForAdmins = false --Az Admin válasz üzenet részének kijelzése (csak az adminoknak)
 Config.ShowReplyMessageForAdmin = false --Az Admin válasz üzenet részének kijelzése (csak neked)
 
-Config.DeleteReport = true --Reportok törlése x idő közönként
-Config.DeleteReportTime = 200000 -- Reportok törlésének az ideje 
-
-
-
 --Parancsok
 Config.ReplyCommand = "r" --Válasz parancs
 Config.ReportCommand = "report" -- Report írás parancs
